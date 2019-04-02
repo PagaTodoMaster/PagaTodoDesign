7120d4d573c3433c882489f62e0ed5cee04a9ff8
Ismael Cruz <icruz@Pagatodo.com>
2019-04-02 16:39:44 -0600

    Primera Version

---
+++ pagatododesign/src/main/java/com/pagatodo/pagatododesign/Test.java
@@ -0,0 +1,17 @@
+package com.pagatodo.pagatododesign;
+
+public class Test {
+    private String nameTes;
+
+    public Test(String nameTes) {
+        this.nameTes = nameTes;
+    }
+
+    public String getNameTes() {
+        return nameTes;
+    }
+
+    public void setNameTes(String nameTes) {
+        this.nameTes = nameTes;
+    }
+}
