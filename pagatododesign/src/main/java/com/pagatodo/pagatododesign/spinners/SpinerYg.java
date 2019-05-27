96684d4f525b495a2bd838f460427d9f4f84bd22
Ismael Cruz <icruz@Pagatodo.com>
2019-05-27 12:41:14 -0500

    Spinner Add

---
+++ pagatododesign/src/main/java/com/pagatodo/pagatododesign/spinners/SpinerYg.java
@@ -0,0 +1,66 @@
+package com.pagatodo.pagatododesign.spinners;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.pagatodo.pagatododesign.R;
+
+public class SpinerYg extends LinearLayout {
+
+    private TextView textHint;
+    private Spinner spinner;
+
+    public SpinerYg(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public SpinerYg(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public SpinerYg(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View viewItem = inflater.inflate(R.layout.spinner_yg,this,false);
+        textHint = viewItem.findViewById(R.id.texthint);
+        spinner = viewItem.findViewById(R.id.spinner_yg);
+
+        if (attrs != null) {
+            TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.SpinerYg,
+                    0, 0);
+            try {
+
+                String resTextHint = a.getString(R.styleable.SpinerYg_textHintSpinner);
+                textHint.setText(resTextHint);
+                textHint.setVisibility(VISIBLE);
+            } finally {
+                a.recycle();
+            }
+        }
+        this.addView(viewItem);
+    }
+
+    public TextView getTextHint() {
+        return textHint;
+    }
+
+    public Spinner getSpinner() {
+        return spinner;
+    }
+}
